078c25d8f4f432206d7d214c8673029247591de4
Tkhangdi <tkhangdi2@gmail.com>
2024-10-17 11:01:11 +0700

    first commit

---
+++ Nhom05_QuanLyShopQuanAo/Controllers/DangNhapController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using System.Configuration;
+using Nhom05_QuanLyShopQuanAo.Models;
+
+namespace Nhom05_QuanLyShopQuanAo.Controllers
+{
+    public class DangNhapController : Controller
+    {
+        public string str;
+        SqlConnection conn;
+        public DangNhapController()
+        {
+            str = ConfigurationManager.ConnectionStrings["QLSHOPQUANAO"].ConnectionString;
+            conn = new SqlConnection(str);
+        }
+        // GET: DangNhap
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string manv,string mk)
+        {
+            if (ModelState.IsValid)
+                conn.Open();
+            string query = "SELECT count(*) FROM NhanVien WHERE TenDangNhap = '"+manv+ "' AND MatKhau = '" + mk + "'";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            int kq = (int)cmd.ExecuteScalar();
+            if (kq > 0)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Invalid Username or Password!";
+                return View();
+            }
+        }
+
+        
+    }
+}
---
+++ Nhom05_QuanLyShopQuanAo/Models/NhanVien.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nhom05_QuanLyShopQuanAo.Models
+{
+    public class NhanVien
+    {
+
+        public string MaNhanVien { get; set; }
+        public string Ho { get; set; }
+        public string Ten { get; set; }
+        public string GioiTinh { get; set; }
+        public string Email { get; set; }
+        public string DienThoai { get; set; }
+        public string ChucVu { get; set; }
+        public string MatKhau { get; set; }
+
+
+    }
+}
